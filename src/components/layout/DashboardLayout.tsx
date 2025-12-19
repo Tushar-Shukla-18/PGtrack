185b7df617f3eb6d8999946fa2842a773dc894e2
Tushar-Shukla-18 <tusharshukla2030@gmail.com>
2025-12-20 01:38:58 +0530

    Update DashboardLayout.tsx

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Sidebar } from "./Sidebar";
 
 interface DashboardLayoutProps {
@@ -5,12 +6,33 @@ interface DashboardLayoutProps {
 }
 
 export function DashboardLayout({ children }: DashboardLayoutProps) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
-    <div className="min-h-screen bg-background">
-      <Sidebar />
-      <main className="lg:pl-64 min-h-screen transition-all duration-300">
-        {children}
-      </main>
-    </div>
-  );
-}
+    <div className="min-h-screen bg-background flex">
+      {/* Desktop Sidebar */}
+      <div className="hidden lg:block">
+        <Sidebar />
+      </div>
+
+      {/* Mobile overlay */}
+      {sidebarOpen && (
+        <div
+          className="fixed inset-0 z-40 bg-black/50 lg:hidden"
+          onClick={() => setSidebarOpen(false)}
+        />
+      )}
+
+      {/* Mobile Sidebar */}
+      <div
+        className={`fixed inset-y-0 left-0 z-50 w-64 bg-background transform transition-transform duration-300 lg:hidden ${
+          sidebarOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <Sidebar onClose={() => setSidebarOpen(false)} />
+      </div>
+
+      {/* Main content */}
+      <div className="flex-1 lg:pl-64">
+        {/* Mobile top bar */}
+        <div className="lg:hidden p-4 b
